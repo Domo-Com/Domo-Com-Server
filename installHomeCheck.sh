8e590a249bae14e4247b2dc92e3785d4d320889b
Domo-Com <38399403+Domo-Com@users.noreply.github.com>
2021-08-27 10:10:29 +0200

    Update installHomeCheck.sh

--- installHomeCheck.sh
+++ installHomeCheck.sh
@@ -1,21 +1,27 @@
+#!/bin/bash
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+Directory="/var/www/yana-server/plugins/HomeTest/"
+echo $tmp_dir
 rm -R -f Domo-Com-Server-master
 rm  -f master.zip
 echo "Getting last version"
-wget https://github.com/Domo-Com/Domo-Com-Server/archive/refs/heads/master.zip
-unzip ./master.zip
-cd  Domo-Com-Server-master/
+wget -P "$tmp_dir" https://github.com/Domo-Com/Domo-Com-Server/archive/refs/heads/master.zip
+cd $tmp_dir
+unzip $tmp_dir/master.zip
+cd  $tmp_dir/Domo-Com-Server-master/
 unzip homecheck-master.zip
 cd homecheck-master
-Directory="/var/www/yana-server/plugins/HomeCheck/"
+
 ### To check if it's not exists
 if [ ! -d "$Directory" ];
 then 
 	echo "create HomeCheck\n"
-	mkdir /var/www/yana-server/plugins/HomeCheck/
+	mkdir "$Directory"
 fi
-echo "Copying Homecheck"
-cp ./* /var/www/yana-server/plugins/HomeCheck/ -f -r
-sudo chown -R www-data:www-data /var/www/yana-server/plugins/HomeCheck/
-sudo chmod -R 775 /var/www/yana-server/plugins/HomeCheck/
+echo "Copying $Directory"
+cp ./* "$Directory" -f -r
+sudo chown -R www-data:www-data "$Directory"
+sudo chmod -R 775 "$Directory"
 sudo usermod -a -G video www-data
+rm -rf $tmp_dir
 echo "Done"
